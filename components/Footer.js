9decd76d366f3e08d585e5c089dec51f30dd42a4
Jefferyagyepong <agyepong9jeff@icloud.com>
2023-10-11 19:58:35 +0000

    newupdates

--- components/Footer.js
+++ components/Footer.js
@@ -94,12 +94,4 @@ export default function Footer() {
       </a>
     </footer>
   );
-  var stylesheet =
-      "https://www.testdome.com/content/source/stylesheets/embed.css",
-    link = document.createElement("link");
-  (link.href = stylesheet),
-    (link.type = "text/css"),
-    (link.rel = "stylesheet"),
-    (link.media = "screen,print"),
-    document.getElementsByTagName("head")[0].appendChild(link);
 }
