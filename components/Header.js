330d5425228d9e6d11ce4092db034e5795544007
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-02-06 11:56:56 +0000

    site updated

--- components/Header.js
+++ components/Header.js
@@ -1,31 +1,32 @@
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 export default function Header() {
   return (
     <header>
       <Link href={"./"}>
-        <Image src={"/jeffery.svg"} width={60} height={80} alt="logo"/>
+        <Image src={"/jeffery.svg"} width={60} height={80} alt="logo" />
       </Link>
+      <div className="right">
+        <section className="top-nav">
+          <input id="menu-toggle" type="checkbox" />
 
-      <section className="top-nav">
-        <input id="menu-toggle" type="checkbox" />
-
-        <label className="menu-button-container" htmlFor="menu-toggle">
-          <div className="menu-button"></div>
-        </label>
-        <ul className="menu">
-          <li>
-            <Link href={"/mentorship"} className="header-nav-link">
-              Mentorship
-            </Link>
-          </li>
-          <li>
-            <Link href={"/sayhello"} className="header-nav-link">
-              Say Hello
-            </Link>
-          </li>
-        </ul>
-      </section>
+          <label className="menu-button-container" htmlFor="menu-toggle">
+            <div className="menu-button"></div>
+          </label>
+          <ul className="menu">
+            <li>
+              <Link href={"/mentorship"} className="header-nav-link">
+                Mentorship
+              </Link>
+            </li>
+            <li>
+              <Link href={"/sayhello"} className="header-nav-link">
+                Say Hello
+              </Link>
+            </li>
+          </ul>
+        </section>
+      </div>
     </header>
   );
 }
