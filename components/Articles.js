bdd4b49a776bad0fe29226f0afbe49a996dad972
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-07-08 01:50:39 +0000

    init

--- components/Articles.js
+++ components/Articles.js
@@ -2,14 +2,60 @@ import Link from "next/link";
 import Image from "next/image";
 export default function Articles() {
     return (
-   
-        <div className="articles">
-          <h3 className="myself-header">Latest articles</h3>
-          <p>
-            I write articles on web development, including links to resources.
-          </p>
+      <div className="articles">
+        <h3 className="myself-header">Latest articles</h3>
+        <p>
+          I write articles on web development, including links to resources.
+        </p>
+        <nav>
+          <Link href={"#image-2"}>
+            <Image
+              src="/arrow-back-circle-outline.svg"
+              alt="Coast Republic"
+              width={20}
+              height={20}
+            />
+          </Link>
+          <Link href={"#image-1"}>
+            <Image
+              src="/arrow-forward-circle-outline.svg"
+              alt="Coast Republic"
+              width={20}
+              height={20}
+            />
+          </Link>
+        </nav>
+        <div className="list-wrapper">
+          <ul className="list">
+            <li className="item">
+              <div className="content">
+                <Image
+                  id={"image-1"}
+                  src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="engineering"
+                  width={580}
+                  height={200}
+                />
+              </div>
+              <h4>Learn Web Developement</h4>
+              <p>Learning web development can be an exciting journey</p>
+            </li>
+            <li className="item">
+              <div className="content">
+                {" "}
+                <Image
+                  id={"image-2"}
+                  src="logo-html5.svg"
+                  alt="html logo"
+                  width={380}
+                  height={400}
+                />
+              </div>
+              <h4>HTML 5</h4>
+              <p>HTML (HyperText Markup Language)</p>
+            </li>
+          </ul>
         </div>
-
-    
+      </div>
     );
 }
--- components/Work.js
+++ components/Work.js
@@ -6,47 +6,56 @@ export default function Work() {
       <h3 className="myself-header">Projects</h3>
       <p>Projects I have worked on and link is available</p>
       <nav>
-        <Image
-          src="/arrow-back-circle-outline.svg"
-          alt="Coast Republic"
-          width={20}
-          height={20}
-        />
-
-        <Image
-          src="/arrow-forward-circle-outline.svg"
-          alt="Coast Republic"
-          width={20}
-          height={20}
-        />
+        <Link href={"#image-2"}>
+          <Image
+            src="/arrow-back-circle-outline.svg"
+            alt="Coast Republic"
+            width={20}
+            height={20}
+          />
+        </Link>
+        <Link href={"#image-1"}>
+          <Image
+            src="/arrow-forward-circle-outline.svg"
+            alt="Coast Republic"
+            width={20}
+            height={20}
+          />
+        </Link>
       </nav>
-
-      <ul className="list">
-        <li className="item">
-          <div className="content">
-            <Image
-        
-              id={"image-1"}
-              src="/backgroundcr1.png"
-              alt="Coast Republic"
-              width={380}
-              height={400}
-            />
-          </div>
-        </li>
-        <li className="item">
-          <div className="content">
-            {" "}
-            <Image
-              id={"image-2"}
-              src="/yo.png"
-              alt="Yo Cho"
-              width={380}
-              height={400}
-            />
-          </div>
-        </li>
-      </ul>
+      <div className="list-wrapper">
+        <ul className="list">
+          <li className="item">
+            <div className="content">
+              <Image
+                id={"image-1"}
+                src="/backgroundcr1.png"
+                alt="Coast Republic"
+                width={380}
+                height={400}
+              />
+            </div>
+            <Link href={"#"} className="mail-link">
+              Visit Website
+            </Link>
+          </li>
+          <li className="item">
+            <div className="content">
+              {" "}
+              <Image
+                id={"image-2"}
+                src="/yo.png"
+                alt="Yo Cho"
+                width={380}
+                height={400}
+              />
+            </div>
+            <Link href={"#"} className="mail-link">
+              Visit Website
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
