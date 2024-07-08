97c5ef4a18670841bbe97d1232e4788b30cbc880
Jefferyagyepong <agyepong9jeff@icloud.com>
2024-07-08 23:44:04 +0000

    init

--- pages/blog/Post.js
+++ pages/blog/Post.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 const Post = ({ post: { title, body, imgUrl, author, date }, index }) => {
   return (
     <div>
       <Image src={imgUrl} alt="post" width={100} height={120} />
       <div className="blog-footer">
-        <h6>{author}</h6>
-        <h6>{date}</h6>
+        <p>{author}</p>
+        <p>{date}</p>
       </div>
       <h1>{title}</h1>
       <p>{body}</p>
