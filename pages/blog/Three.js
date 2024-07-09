9ea469870cb4e55a3d7c3b4610fd045d7fe8b6dc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-09 14:12:46 +0000

    Update Three.js
    Meta updates

--- pages/blog/Three.js
+++ pages/blog/Three.js
@@ -35,7 +35,8 @@ const Three = () => {
         />
         <meta
           name="twitter:image"
-          content="https://jefferyagyepong-io.vercel.app/public/card.png"
+          content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              
         />
         <meta property="og:title" content="Sass article" />
         <meta property="og:type" content="blog website" />
@@ -43,7 +44,8 @@ const Three = () => {
           property="og:url"
           content="https://jefferyagyepong-io.vercel.app/blog/Three"
         />
-        <meta property="og:image" content="/https://jefferyagyepong-io.vercel.app/public/card.png" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
         <meta name="google-adsense-account" content="ca-pub-2474265697377160" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
