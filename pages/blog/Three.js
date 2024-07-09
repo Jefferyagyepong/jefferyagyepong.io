20c5c1ec26c93afe2a310d22c05ac065e3bc824d
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-09 02:39:23 +0000

    Update Three.js

--- pages/blog/Three.js
+++ pages/blog/Three.js
@@ -35,7 +35,7 @@ const Three = () => {
         />
         <meta
           name="twitter:image"
-          content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          content="/public/card.png"
         />
         <meta property="og:title" content="Sass article" />
         <meta property="og:type" content="blog website" />
