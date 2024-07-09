299159ed273c3159a9cad3f68901f386716c2dac
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-09 14:07:58 +0000

    Update index.js
    Meta data updates

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -25,7 +25,8 @@ export default function Blog() {
         />
         <meta
           name="twitter:image"
-          content="https://jefferyagyepong-io.vercel.app/public/card.png"
+          content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              
         />
 
         <meta property="og:title" content="Web development" />
@@ -34,7 +35,8 @@ export default function Blog() {
           property="og:url"
           content="https://jefferyagyepong-io.vercel.app/blog/"
         />
-        <meta property="og:image" content="https://jefferyagyepong-io.vercel.app/public/card.png" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              />
         <meta name="google-adsense-account" content="ca-pub-2474265697377160" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -76,7 +78,8 @@ export default function Blog() {
               <div className="content">
                 <Image
                   id={"image-1"}
-                  src="/public/"
+                  src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              
                   alt="engineering"
                   width={380}
                   height={200}
