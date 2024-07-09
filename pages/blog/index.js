09ac9eb22fad5185a24548ccd3eeabb0d6e9ca25
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-09 02:36:26 +0000

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -25,7 +25,7 @@ export default function Blog() {
         />
         <meta
           name="twitter:image"
-          content="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          content="/public/card.png"
         />
 
         <meta property="og:title" content="web development blog" />
@@ -76,7 +76,7 @@ export default function Blog() {
               <div className="content">
                 <Image
                   id={"image-1"}
-                  src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  src="/public/"
                   alt="engineering"
                   width={380}
                   height={200}
