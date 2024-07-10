cc89ddbb4a98455093346eb7d943111ac03a17e5
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-07-10 14:52:01 +0000

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -16,7 +16,7 @@ export default function Contact() {
           property="og:url"
           content="https://jefferyagyepong-io.vercel.app"
         />
-        <meta property="og:image" content="https://i.postimg.cc/DzR6JLQL/lee.png"   />
+        <meta property="og:image" content="https://i.postimg.cc/Bv2qz8cs/IMG-0568.jpg"/>
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="author" content="Jeffery Agyepong" />
