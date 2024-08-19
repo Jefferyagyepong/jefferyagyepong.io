ac7922cbc27afa306f964aaf8bf7634ef5c4a940
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-19 20:34:37 +0000

    Update Introduction.js

--- components/Introduction.js
+++ components/Introduction.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
  function Introduction() {
   return (
-    <div className="container">
+    <div className="fluid-container">
       <h2 className="intro-message">I design and code beautifully simple things, and I love what I do</h2>
       <p>Want to know how?</p>
       <Link href="mailto:agyepong9jeff@icloud.com" className="mail-link">Contact Me</Link>
