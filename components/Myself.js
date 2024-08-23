94cda35011c042b22e0ddaf8cc776c21fdee2df0
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-23 12:35:51 +0000

    Update Myself.js

--- components/Myself.js
+++ components/Myself.js
@@ -3,7 +3,7 @@ import Image from "next/image";
  function Myself() {
   return (
     <div className="fluid-container">
-      <h4 className="myself-header">I am a Web Developer</h4>
+      <h4 className="text-center">I am a Web Developer</h4>
       <Link href="/aboutme" className="slate-blue">
         Read more about me <Image src={"/arrow-forward-sharp.svg"} width={20} height={20} alt="forward arrow" />
       </Link>
