ff71ca77021ba6718a85a0bce65b097a65149d38
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-28 22:54:58 +0000

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Image from "next/image";
-const Post = ({ post: { title, body, imgUrl }, index }) => {
+const Post = ({ post: { title,imgUrl,
+paragraph_1,
+paragraph_2,
+paragraph_3,
+paragraph_4,
+paragraph_5,
+paragraph_6,
+paragraph_7,
+paragraph_8,
+paragraph_9,
+paragraph_10,
+paragraph_11, }, index }) => {
   return (
     <div className="fluid-container-align-left">
       <h1 >{title}</h1>
@@ -10,8 +21,18 @@ const Post = ({ post: { title, body, imgUrl }, index }) => {
         width={80}
         height={78}
       />
-      <p className="article-para">{body}</p>
-
+      <p className="article-para">{paragraph_1}</p>
+  <p className="article-para">{paragraph_1}</p>
+  <p className="article-para">{paragraph_2}</p>
+  <p className="article-para">{paragraph_3}</p>
+  <p className="article-para">{paragraph_4}</p>
+  <p className="article-para">{paragraph_5}</p>
+  <p className="article-para">{paragraph_6}</p>
+  <p className="article-para">{paragraph_7}</p>
+  <p className="article-para">{paragraph_8}</p>
+  <p className="article-para">{paragraph_9}</p>
+  <p className="article-para">{paragraph_10}</p>
+  <p className="article-para">{paragraph_11}</p>
     </div>
   );
 };
