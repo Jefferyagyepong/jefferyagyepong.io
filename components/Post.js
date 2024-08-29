834172a233b36a902c595c5e0d9fb569c82ba098
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-29 08:45:43 +0000

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
-const Post = ({ post: { title,body,imgUrl }, index }) => {
+const Post = ({ post: { title,pOne,pTwo,pThree,pFour
+pFive,pSix,pSeven,pEight,pNine
+pTen,imgUrl }, index }) => {
   return (
     <div className="fluid-container-align-left">
       <h1 >{title}</h1>
@@ -10,7 +12,17 @@ const Post = ({ post: { title,body,imgUrl }, index }) => {
         width={80}
         height={78}
       />
-      <p className="article-para">{body}</p>
+      <p className="article-para">{pOne}</p>
+       <p className="article-para">{pTwo}</p>
+       <p className="article-para">{pThree}</p>
+       <p className="article-para">{pFour}</p>
+       <p className="article-para">{pFive}</p>
+       <p className="article-para">{pSix}</p>
+       <p className="article-para">{pSeven}</p>
+       <p className="article-para">{pEight}</p> 
+       <p className="article-para">{pNine}</p>
+       <p className="article-para">{pTen}</p>
+       <p className="article-para">{pEleven}</p>
   
  
 
