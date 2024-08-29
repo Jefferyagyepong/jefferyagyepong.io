4fa11009b1bd946f0ba07a248790a8ad566992f7
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2024-08-29 08:39:23 +0000

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Image from "next/image";
-const Post = ({ post: { title,body,imgUrl }, index }) => {
+const Post = ({ post: { title,pOne,pTwo,pThree,
+pFour,
+pFive,
+pSix,
+pSeven,
+pEight,
+pNine,
+pTen,
+pEleven,imgUrl }, index }) => {
   return (
     <div className="fluid-container-align-left">
       <h1 >{title}</h1>
