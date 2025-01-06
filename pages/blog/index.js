8138e8182185d3c28cef88095176e4cbec59b3be
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-06 21:12:11 +0400

    Update index.js

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -71,7 +71,7 @@ export default function Blog() {
       </Head>
       <main>
         <Header />
-        <div className="fluid-container">
+        <div className="fluid-container-flex">
           <Image
             id={"image-1"}
             src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
