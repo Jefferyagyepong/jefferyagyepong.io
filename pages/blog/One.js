5fcf28a8b2acafe8d4e78a3216131fc9692b019e
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-07 19:27:12 +0400

    Update One.js

--- pages/blog/One.js
+++ pages/blog/One.js
@@ -117,6 +117,34 @@ const One = () => {
             </div>
           </nav>
         </div>
+                 <div className="blog-footer">
+            <span>
+              By: Jeffery Agyepong |{" "}
+              <Image
+                src={"/lee.png"}
+                width={44}
+                height={44}
+                alt="jeffery agyepong"
+              />
+            </span>
+            <span className="blog-footer-text">February 03, 2024</span>
+          </div>
+          <div className="fluid-container">
+            <h5>share on social media</h5>
+            <Link
+              href="https://twitter.com/intent/tweet?text=http://jefferyagyepong-io.vercel.app/blog/"
+              target="_blank"
+              className="twitter-share-button"
+            >
+              <Image
+                src={"/logo-twitter.svg"}
+                width={20}
+                height={20}
+                alt="icon"
+              />{" "}
+            </Link>{" "}
+          </div>
+        </div>
         <div className="fluid-container">
           <h4>Resources</h4>
           <ul>
@@ -145,34 +173,7 @@ const One = () => {
             </li>
           </ul>
         </div>
-           <div className="blog-footer">
-            <span>
-              By: Jeffery Agyepong |{" "}
-              <Image
-                src={"/lee.png"}
-                width={44}
-                height={44}
-                alt="jeffery agyepong"
-              />
-            </span>
-            <span className="blog-footer-text">February 03, 2024</span>
-          </div>
-          <div className="fluid-container">
-            <h5>share on social media</h5>
-            <Link
-              href="https://twitter.com/intent/tweet?text=http://jefferyagyepong-io.vercel.app/blog/"
-              target="_blank"
-              className="twitter-share-button"
-            >
-              <Image
-                src={"/logo-twitter.svg"}
-                width={20}
-                height={20}
-                alt="icon"
-              />{" "}
-            </Link>{" "}
-          </div>
-        </div>
+  
       </main>
     </>
   );
