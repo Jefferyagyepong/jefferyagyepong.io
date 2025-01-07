61b240a1908ee550324ded45d54a5ebf320fe412
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-01-07 18:54:43 +0400

    Update Post.js

--- components/Post.js
+++ components/Post.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Image from "next/image";
-const Post = ({ post: { title,p1, p2,p3,p4,p5,p6,p7,p8,p9,p10,imgUrl }, index }) => {
+const Post = ({ post: { title,p1, p2,p3,p4,p5,p6,p7,p8,p9,p10,imgUrl,phead}, index }) => {
   return (
     <div className="fluid-container-align-left">
       <h3>{title}</h3>
       <Image src={imgUrl} alt="post" width={80} height={78} />
-      <p className="article-para">{p1}</p>
-      <p className="article-para">{p2}</p>
-      <p className="article-para">{p3}</p>
-      <p className="article-para">{p4}</p>
-      <p className="article-para">{p5}</p>
-      <p className="article-para">{p6}</p>
-      <p className="article-para">{p7}</p>
-      <p className="article-para">{p8}</p>
-      <p className="article-para">{p9}</p>
-      <p className="article-para">{p10}</p>
+      <p>{p1}</p>
+      <p>{p2}</p>
+      <p>{p3}</p>
+      <p>{p4}</p>
+      <p>{p5}</p>
+      <p>{p6}</p>
+      <p>{p7}</p>
+      <p>{p8}</p>
+      <p>{p9}</p>
+      <p>{p10}</p>
+       <p>{p11}</p>
+       <p>{p12}</p>
+       <p>{p13}</p>
+      <h6>{phead}</h6>
     </div>
   );
 };
