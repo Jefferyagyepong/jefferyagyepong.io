b8735f3549aceb6904b44190d896a4102d8f2662
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-04-16 10:33:59 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,7 @@ export default function Home() {
         <Header />
         <Introduction />
         <Myself />
-        <Articles />
+      
         <Work />
         <Footer />
       </main>
