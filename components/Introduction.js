53e9391d92a4a2ad4e8bcdc4bd4b7c519b64bb0b
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-22 15:27:48 +0000

    Update Introduction.js

--- components/Introduction.js
+++ components/Introduction.js
@@ -1,53 +1,15 @@
 
 import Link from "next/link";
-
+import Image from "next/image";
  function Introduction() {
   return (
-    <div className="fluid-container-flex">
-              <div>
-        <Link href={"./"}>
-          <h3 className="white">Jeffery</h3>
-        </Link>
-      </div>
-      <div>
-        <Link
-          href="https://twitter.com/jeffagyepong"
-          target="_blank"
-          className="footer-link-item"
-        >
-          <Image src={"/logo-twitter.svg"} width={33} height={33} alt="icon" />{" "}
-        </Link>{" "}
-        <Link
-          href="https://github.com/Jefferyagyepong"
-          target="_blank"
-          className="footer-link-item"
-        >
-          <Image src={"/logo-github.svg"} width={33} height={33} alt="icon" />{" "}
-        </Link>{" "}
-        <Link
-          href="https://www.linkedin.com/in/jeffery-agyepong-943ba0234"
-          target="_blank"
-          className="footer-link-item"
-        >
-          <Image src={"/logo-linkedin.svg"} width={33} height={33} alt="icon" />{" "}
-        </Link>{" "}
-        <Link
-          href="https://www.instagram.com/jefferyagyepong_/"
-          target="_blank"
-          className="footer-link-item"
-        >
-          <Image
-            src={"/logo-instagram.svg"}
-            width={33}
-            height={33}
-            alt="icon"
-          />{" "}
-        </Link>
-      </div>
-      <h2 className="intro-message">I design and code beautifully simple things, and I love what I do</h2>
-      <p className="text-center">Want to know how?</p>
-      <Link href="mailto:agyepong9jeff@icloud.com" className="mail-link">Contact Me</Link>
-    </div>
+   
+     <div className="fluid-container">
+<div className="profile-picture">
+  <Image src ={"/IMG_3237.jpeg"} width={40} height={50}/>
+  
+  </div>
+</div>
   );
 }
 export default Introduction;
