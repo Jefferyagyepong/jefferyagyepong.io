6680ebeb5b1e8ab691cb30e619dcd026a3703fea
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-22 18:09:28 +0000

    Update Myself.js

--- components/Myself.js
+++ components/Myself.js
@@ -3,9 +3,11 @@ import Image from "next/image";
  function Myself() {
   return (
     <div className="fluid-container-row">
+      <div className="bg-picture">
+           <Image src={"/https://unsplash.com/photos/black-and-white-laptop-computer-XopauR-Nagk"} width={220} height={320} alt="web dev" />
+        </div>
       
-      
-       <Image src={"/https://unsplash.com/photos/black-and-white-laptop-computer-XopauR-Nagk"} width={220} height={320} alt="web dev" />
+    
       <div className="bg-purple">
         <h3>Career guidance in Web Development</h3>
        <p> ⏱️ 724hrs</p>
