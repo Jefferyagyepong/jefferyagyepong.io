918abb0b76b2b5799d450e79e507f80ba386eae5
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-22 15:55:44 +0000

    Update Introduction.js

--- components/Introduction.js
+++ components/Introduction.js
@@ -14,6 +14,41 @@ import Image from "next/image";
 <h5>Web Developer </h5>
 <p>Are you new to the world of building websites? Let me guide 
     you on journey to becoming a skilled professional</p>
+    <div>
+        <Link
+          href="https://twitter.com/jeffagyepong"
+          target="_blank"
+          className="footer-link-item"
+        >
+          <Image src={"/logo-twitter.svg"} width={93} height={93} alt="icon" />{" "}
+        </Link>{" "}
+        <Link
+          href="https://github.com/Jefferyagyepong"
+          target="_blank"
+          className="footer-link-item"
+        >
+          <Image src={"/logo-github.svg"} width={33} height={33} alt="icon" />{" "}
+        </Link>{" "}
+        <Link
+          href="https://www.linkedin.com/in/jeffery-agyepong-943ba0234"
+          target="_blank"
+          className="footer-link-item"
+        >
+          <Image src={"/logo-linkedin.svg"} width={33} height={33} alt="icon" />{" "}
+        </Link>{" "}
+        <Link
+          href="https://www.instagram.com/jefferyagyepong_/"
+          target="_blank"
+          className="footer-link-item"
+        >
+          <Image
+            src={"/logo-instagram.svg"}
+            width={33}
+            height={33}
+            alt="icon"
+          />{" "}
+        </Link>
+      </
 </div>
   );
 }
