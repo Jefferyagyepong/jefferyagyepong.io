979ac6db75a7e6e83cc0e1d9976ce6e85d991b37
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-22 15:43:43 +0000

    Update Introduction.js

--- components/Introduction.js
+++ components/Introduction.js
@@ -4,11 +4,16 @@ import Image from "next/image";
  function Introduction() {
   return (
    
-     <div className="fluid-container">
+     <div className="fluid-container-flex">
 <div className="profile-picture">
   <Image src ={"/IMG_3237.jpeg"} width={40} height={50}/>
   
   </div>
+<h2>Jeffery Agypong</h2>
+<span>Internet</span>
+<h5>Web Developer </h5>
+<p>Are you new to the world of building websites? Let me guide 
+    you on journey to becoming a skilled professional</p>
 </div>
   );
 }
