1c18394b3d27db0cb138578837c5c1a7311465cc
Jeffery Agyepong <102326733+Jefferyagyepong@users.noreply.github.com>
2025-09-23 08:47:17 +0000

    init

--- components/Myself.js
+++ components/Myself.js
@@ -4,6 +4,7 @@ import Image from "next/image";
   return (
     <div className="fluid-container-row">
       <div className="bg-picture">
+        <Image src={"https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?q=80&w=702&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} width={150} height={200}/>
          
         </div>
       
